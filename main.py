c57786d3717b18fda02879218904219d487bed53
chukhraiartur <chukhraiartur@gmail.com>
2023-02-14 18:09:43 +0200

    fix: fixed typos and write documentation under 'get_depth_results()' function

--- main.py
+++ main.py
@@ -55,6 +55,16 @@ def get_related_questions(query: str, domain: str, country: str, lang: str, api_
 
     if depth_limit:
         def get_depth_results(token, depth, api_key):
+            ''' This function allows you to extract more data from People Also Ask.
+            
+            The function takes the following arguments:
+            
+            - `token` - allows access to additional related questions;
+            
+            - `depth` - limits the input depth for each related question;
+            
+            - `api_key` - your SerpApi API key. '''
+
             depth_params = {
                 'api_key': api_key,
                 'engine': 'google_related_questions',
@@ -65,7 +75,7 @@ def get_related_questions(query: str, domain: str, country: str, lang: str, api_
             depth_results = depth_search.get_dict()
             
             related_questions_results.extend([result['question'] for result in depth_results.get('related_questions', [])])
-
+            
             if depth > 1:
                 for question in depth_results.get('related_questions', []):
                     if question.get('next_page_token'):
@@ -97,30 +107,36 @@ def save_to_txt(data: dict):
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='SEO Keyword Research Options')
-    parser.add_argument('-q','--query', metavar='', required=True, help='search query')
-    parser.add_argument('-e','--engines', metavar='', required=True, help='choice of engines to extract: Autocomplete (auto-complete), Related Searches (related-searches), People Also Ask (related-questions)')
-    parser.add_argument('-dl','--depth-limit', metavar='', required=False, type=int, default=0, help='depth limit for People Also Ask')
-    parser.add_argument('-st','--save-to', metavar='', required=False, default='CSV', help='saves the results in the current directory in the selected format (CSV, JSON, TXT)')
-    parser.add_argument('-ak','--api-key', metavar='', required=False, default='5868ece26d41221f5e19ae8b3e355d22db23df1712da675d144760fc30d57988', help='your SerpApi API key')
-    parser.add_argument('-gd','--domain', metavar='', required=False, default='google.com', help='google domain')
-    parser.add_argument('-gl','--country', metavar='', required=False, default='us', help='country of the search')
-    parser.add_argument('-hl','--lang', metavar='', required=False, default='en', help='language of the search')
+    parser = argparse.ArgumentParser(
+        prog='SerpApi SEO Keyword Research Tool', 
+        description='Extract keywrods from: Google Autocomplete, People Also Ask, and People Also Search and saves data to CSV/JSON/TXT.', 
+        epilog='Found a bug? Open issue: https://github.com/chukhraiartur/seo-keyword-research-tool/issues'
+    )
+    parser.add_argument('-q','--query', metavar='', required=True, help='Search query (required).')
+    parser.add_argument('-e','--engines', metavar='', required=False, default='all', choices=['ac', 'rs', 'rq'], help='Choices of engines to extract: Autocomplete (ac), Related Searches (rs), People Also Ask (rq). All engines are selected by default.')
+    parser.add_argument('-dl','--depth-limit', metavar='', required=False, type=int, default=0, help='Depth limit for People Also Ask. Default is %(default)i, first 2-4 results.')
+    parser.add_argument('-st','--save-to', metavar='', required=False, default='CSV', help='Saves the results in the current directory in the selected format (CSV, JSON, TXT). Default %(default)s.')
+    parser.add_argument('-ak','--api-key', metavar='', required=False, default='5868ece26d41221f5e19ae8b3e355d22db23df1712da675d144760fc30d57988', help='Your SerpApi API key: https://serpapi.com/manage-api-key. Default is a test API key to test CLI.')
+    parser.add_argument('-gd','--domain', metavar='', required=False, default='google.com', help='Google domain: https://serpapi.com/google-domains. Default %(default)s.')
+    parser.add_argument('-gl','--country', metavar='', required=False, default='us', help='Country of the search: https://serpapi.com/google-countries. Default %(default)s.')
+    parser.add_argument('-hl','--lang', metavar='', required=False, default='en', help='Language of the search: https://serpapi.com/google-languages. Default %(default)s.')
 
     args = parser.parse_args()
-
+    
     data = {}
-
-    if 'auto-complete' in args.engines:
+    
+    if 'all' in args.engines:
         data['auto_complete'] = get_auto_complete(args.query, args.country, args.lang, args.api_key)
-
-    if 'related-searches' in args.engines:
         data['related_searches'] = get_related_searches(args.query, args.domain, args.country, args.lang, args.api_key)
-
-    if 'related-questions' in args.engines:
+        data['related_questions'] = get_related_questions(args.query, args.domain, args.country, args.lang, args.api_key, args.depth_limit)
+    elif 'ac' in args.engines:
+        data['auto_complete'] = get_auto_complete(args.query, args.country, args.lang, args.api_key)
+    elif 'rs' in args.engines:
+        data['related_searches'] = get_related_searches(args.query, args.domain, args.country, args.lang, args.api_key)
+    elif 'rq' in args.engines:
         data['related_questions'] = get_related_questions(args.query, args.domain, args.country, args.lang, args.api_key, args.depth_limit)
     
-    print(json.dumps(data, indent=2, ensure_ascii=False))
+    # print(json.dumps(data, indent=2, ensure_ascii=False))
 
     if args.save_to:
         print(f'Saving data in {args.save_to.upper()} format...')
